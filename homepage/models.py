2bdbd263e1f35c0e8d3205d5fbcd6c2d63e8d2b3
katty87 <esenenkova@gmail.com>
2020-07-22 22:37:47 +0300

    Add question votes

--- homepage/models.py
+++ homepage/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.db.models import Sum
 
 
 class UserProfile(models.Model):
@@ -16,10 +17,26 @@ class Question(models.Model):
     # user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='question_user')
     create_date = models.DateTimeField()
 
+    def vote_count(self):
+        cnt = QuestionVote.objects.filter(question_id=self.id).aggregate(Sum('value')).get('value__sum')
+        return cnt if cnt else 0
+
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.CharField(max_length=4096)
     # user = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answer_user')
     is_correct = models.BooleanField()
+
+
+class AnswerVote(models.Model):
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    value = models.IntegerField(default=0)
+
+
+class QuestionVote(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    value = models.IntegerField(default=0)
+
+
 
