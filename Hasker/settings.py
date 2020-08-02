687f7a86bb7d832fdb2ebb1da2af445a051da929
katty87 <esenenkova@gmail.com>
2020-08-02 16:07:24 +0300

    Add answer voting

--- Hasker/settings.py
+++ Hasker/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '!q^vwef^38%+1a_f@icc73q6by#==)=zca)sgp)tlq&1owzox)'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -69,12 +67,16 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'homepage.context_processors.trending_questions',
             ],
+
+            'libraries': {
+                'homepage_filters': 'homepage.templatetags.homepage_filters',
+
+            }
         },
     },
 ]
 
 WSGI_APPLICATION = 'Hasker.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
@@ -85,7 +87,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -105,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 AUTH_PROFILE_MODULE = 'homepage.UserProfile'
 
 # Internationalization
@@ -120,7 +120,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
