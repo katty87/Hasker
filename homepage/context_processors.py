7988164b009ffe114f869a0046e88a70289ce459
katty87 <esenenkova@gmail.com>
2020-08-08 20:27:57 +0300

    Redesign trending questions

--- homepage/context_processors.py
+++ homepage/context_processors.py
@@ -1,6 +1,11 @@
+from django.db.models import Sum
+from django.db.models.functions import Coalesce
+
 from .models import Question
 
 
 def trending_questions(request):
-    trending_question_list = Question.objects.all()[:5]
+    trending_question_list = Question.objects.annotate(vote_sum=Coalesce(Sum('questionvote__value'), 0)). \
+            filter(vote_sum__gt=0). \
+            order_by('-vote_sum', 'create_date').values('id', 'header', 'vote_sum')[:5]
     return {'trending_question_list': trending_question_list}
