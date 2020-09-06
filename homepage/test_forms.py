847bcc5f9c725a3731b0c7e66b2bf6280a5b4b26
katty87 <esenenkova@gmail.com>
2020-09-06 17:03:08 +0300

    add form tests

---
+++ homepage/test_forms.py
@@ -0,0 +1,32 @@
+import pytest
+from django.test import TestCase
+
+from homepage.forms import SignUpForm, UserSettings, AddQuestionForm, QuestionDetailForm
+
+
+class SignUpFormTest(TestCase):
+    def test_signup_form_avatar_help_text(self):
+        form = SignUpForm()
+        self.assertEqual(form.fields['avatar'].help_text, 'Load picture up to 1MB')
+
+
+class UserSettingsFormTest(TestCase):
+    def test_signup_form_avatar_help_text(self):
+        form = UserSettings()
+        self.assertEqual(form.fields['avatar'].help_text, 'Load picture up to 1MB')
+
+
+class AddQuestionFormTest(TestCase):
+    def test_empty_content(self):
+        form = AddQuestionForm(data={'content': ''})
+        self.assertFalse(form.is_valid())
+
+    def test_too_small_content(self):
+        form = AddQuestionForm(data={'content': 'my question'})
+        self.assertFalse(form.is_valid())
+
+
+class QuestionDetailFormTest(TestCase):
+    def test_empty_content(self):
+        form = QuestionDetailForm(data={'answer_text': 'my answer'})
+        self.assertFalse(form.is_valid())
