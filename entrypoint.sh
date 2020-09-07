525ea58ab7b49de2b8578a21dd60992827bcdd25
katty87 <esenenkova@gmail.com>
2020-09-07 18:51:09 +0300

    add entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 python3 manage.py flush --no-input
 python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:80
+python3 manage.py test
+if [[ $? -eq 0 ]]
+then
+    python3 manage.py runserver 0.0.0.0:80
+fi;
