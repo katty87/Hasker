2588220325aaae2ade571819b134e2c1c407e51b
katty87 <esenenkova@gmail.com>
2020-10-11 21:26:39 +0300

    add admin.py to user app

--- user/admin.py
+++ user/admin.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from main.models import UserProfile, Question, Answer, Tag
+from user.models import UserProfile
 
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = UserAdmin.list_display + ('avatar',)
-
-
-admin.site.register(Tag)
-admin.site.register(Question)
-admin.site.register(Answer)
-
