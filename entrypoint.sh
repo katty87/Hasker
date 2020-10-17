02ac6313139232cb4f4562437970de9d3043d96c
katty87 <esenenkova@gmail.com>
2020-10-17 13:45:53 +0300

    Add --no-input parameter

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 python3 manage.py flush --no-input
-python3 manage.py migrate
-python3 manage.py test
+python3 manage.py migrate --no-input
+python3 manage.py test --no-input
 if [[ $? -eq 0 ]]
 then
     python3 manage.py runserver 0.0.0.0:80
